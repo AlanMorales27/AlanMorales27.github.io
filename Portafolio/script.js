be815b5efa35cb7c66f214a3c7e8b29220493f1f
AlanMorales27 <117939838+AlanMorales27@users.noreply.github.com>
2022-12-08 22:52:49 -0500

    comit 3

---
+++ Portafolio/script.js
@@ -0,0 +1,9 @@
+//Funcion responsive
+function responsiveMenu(){
+    var x = document.getElementById("nav");
+    if(x.className==""){
+        x.className = "responsive"
+    } else {
+        x.className = "";
+    }
+}
